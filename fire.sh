e342c1f4b5878c8b02db75be4657edb235604597
Cplus Shen <cplus.shen@gmail.com>
2025-02-26 13:49:19 +0800

    [fire.sh] change the mail format.

--- fire.sh
+++ fire.sh
@@ -1,16 +1,22 @@
 #!/bin/bash -x
-OUTPUT=attachment.md
+OUTPUT=attachment.txt
 RECIPIENT=cplus.shen@gmail.com
 
-echo -e "= Daily Report $(date)\n" | tee $OUTPUT
-echo -e "== US 10 Year Bond Yield Rate" | tee -a $OUTPUT
+echo -e "Daily Report - $(date)" | tee $OUTPUT
+echo -e "\nUS 10 Year Bond Yield Rate" | tee -a $OUTPUT
+echo -e "==========================" | tee -a $OUTPUT
 python bond/us-10-bond-yield.py | tee -a $OUTPUT
 
-echo -e "\n== USD to NTD Exchange Rate" | tee -a $OUTPUT
+echo -e "\nUSD to NTD Exchange Rate" | tee -a $OUTPUT
+echo -e "==========================" | tee -a $OUTPUT
 python currency/usd_to_ntd_exchange.py | tee -a $OUTPUT
 
-echo -e "\n== TW Stock" | tee -a $OUTPUT
+echo -e "\nTW Stock" | tee -a $OUTPUT
+echo -e "==========================" | tee -a $OUTPUT
 python stock/taiwan_stock_price.py | tee -a $OUTPUT
 
 # Send report
 python sendmail-ses.py -f $OUTPUT -m $RECIPIENT
+
+# clean up
+# rm -f $OUTPUT
